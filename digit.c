2b22ca26c462704e90a31753e4a9c000eaf7fcd0
Abinayaatchaya <30333769+Abinayaatchaya@users.noreply.github.com>
2017-07-21 15:19:39 +0530

    digit.c

---
+++ digit.c
@@ -0,0 +1,15 @@
+#inclide<stdio.h>
+int main()
+{
+int num,temp;count=0;
+printf("enter the number");
+scanf("%d",&num);
+temp=num;
+while(temp!=0)
+{
+n=n/10;
+count++;
+}
+printf("%d%d",num,count);
+return 0;
+}
