e4364a9a9b61ef40810162744d5dd7c21714063e
Abinayaatchaya <30333769+Abinayaatchaya@users.noreply.github.com>
2017-07-21 18:03:58 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int num,flag;
+printf("enter the number");
+scanf("%d",&num);
+for(i=0;i<n;i++)
+{
+if(n%i==0)
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("the num is prime number");
+}
+else
+{
+printf("%d is not a prime number");
+}
+return 0;
+}
