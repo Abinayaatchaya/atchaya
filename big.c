fa47c423e709ed0f1bb5d41b31269135865d5395
Abinayaatchaya <30333769+Abinayaatchaya@users.noreply.github.com>
2017-07-21 11:12:35 +0530

    big.c

---
+++ big.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a&b&c);
+if((a>b)&&(a>c))
+{
+printf("a is big");
+}
+else if(b>c)
+{
+printf("b ia big");
+}
+else
+{
+printf("c ia big");
+}
+}
