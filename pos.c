c8e1ec3c9292d5f58c46a40880bfe8d69ffd94db
Abinayaatchaya <30333769+Abinayaatchaya@users.noreply.github.com>
2017-07-21 10:23:30 +0530

    pos.c

---
+++ pos.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&number);
+if(number>0)
+printf("%d is a positive number \n",number);
+else
+printf("%d is a negative number \n",number);
+}
